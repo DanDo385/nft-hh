3c77aea03fa8d26c1b0327064242e86dde12f633
Daniel Magro <dando385@yahoo.com>
2024-05-15 15:28:53 -0400

    removed ownable import

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,18 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFT is ERC721, Ownable {
+contract NFT is ERC721URIStorage {
+    address public owner;
     uint256 public tokenCount;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
     
-    constructor() ERC721("NFT Collection", "NFTC") {}
+    constructor() ERC721("NFT Collection", "NFTC") {
+        owner = msg.sender;
+    }
 
     function mint(string memory tokenURI) external onlyOwner returns (uint256) {
         tokenCount++;
-        _mint(msg.sender, tokenCount);
+        _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, tokenURI);
         return tokenCount;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        owner = newOwner;
     }
 }
